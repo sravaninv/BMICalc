ada7f0ae0d49d2b1bcc3be5c60e5c1b04da0d2f0
Sravani <Sravani@bhargavs-mbp>
2017-03-04 20:42:04 -0600

    Initial Commit

---
+++ BMICalc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BMICalc
+//
+//  Created by Sravani on 3/4/17.
+//  Copyright © 2017 sira. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
